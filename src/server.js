8dae7f1f4b0028aac5fe9dcbd784958cd0979724
Matt Oestreich <matthewpoestreich@gmail.com>
2020-08-09 03:23:09 -0500

    init

--- src/server.js
+++ src/server.js
@@ -11,20 +11,33 @@ const publicPath = path.resolve(__dirname, "../public");
 const staticPath = express.static(publicPath);
 
 const { log } = console;
-const getSerialPortReading = () => Math.floor(Math.random() * 9999);
+
+const { floor, random } = Math;
+const getSerialPortReading = () => floor(random() * 9999);
+
+/**
+ * Express app & websoket handlers/app routes
+ */
 
 app.use('/', staticPath);
+
+// So we can clearInterval @ wss.on("close", ...)
+let interval;
 
 wss.on("connection", (socket) => {
   // Immediately say hi when connection is established
   socket.send("Hello from websocket server!");
 
   // Send our client an updated "reading" every 200ms
-  setInterval(() => socket.send(getSerialPortReading()), 200);
+  interval = setInterval(() => socket.send(getSerialPortReading()), 200);
 
   socket.on("message", (message) => {
     log(`Message received from client: ${message}`);
   });
+});
+
+wss.on("close", (_socket, _req) => {
+  interval && clearInterval(interval);
 });
 
 // 404 route - SHOULD BE LAST
